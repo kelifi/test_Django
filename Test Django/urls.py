07a4876f0de01c30bc5ff1ac13aa340707a092ce
kelifi <78826865+kelifi@users.noreply.github.com>
2023-03-12 20:23:00 +0100

    Delete Test Django directory

--- Test Django/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import UserList, UserDetails
-
-urlpatterns = [
-    path('users/', UserList.as_view(), name='user-list'),
-    path('users/<int:pk>/', UserDetails.as_view(), name='user-details'),
-]
