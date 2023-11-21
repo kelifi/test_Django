b9e7aeab69de986628931195f98896a59b920926
Houssem Khelifi <khelifi.houssem@favizone.com>
2023-11-21 16:01:12 +0100

    Revert "Merge pull request #2 from kelifi/develop"
    This reverts commit 149fe418ef9b4c56917fd60ec70363bf1fa872be, reversing
    changes made to cda91b46c61331dcb74a521f51a1fc60347777cc.

--- urls.py
+++ urls.py
@@ -1,20 +1,9 @@
 from django.urls import path
-
 from .views import UserList, UserDetails
 
 urlpatterns = [
     path('users/', UserList.as_view(), name='user-list'),
     path('users/<int:pk>/', UserDetails.as_view(), name='user-details'),
 ]
-
-
 def afif(name):
     return name
-
-
-def somme(a, b):
-    return a + b
-
-
-def exit():
-    return 8
